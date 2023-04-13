715df71aec19a6726d99b0e20fc13f07a96fd10f
ogizesh <39046371+ogizesh@users.noreply.github.com>
2023-04-13 11:41:18 +0300

    Update function.js

--- function.js
+++ function.js
@@ -5,14 +5,44 @@ const gliderecip = String(in_recip.value) ?? "";
 const glideorder = String(in_order.value) ?? "";
 
 const secret = 'li86qXzVKltU3IOwrGddCtzoB6iVJ2BIHAUOVc6732tTtzOJunmkvxaqqyCci9rkIhzZPkVDp5MR46BH7QEyxt8Kqsz5yejPdiyia9n190m2PimUUJ24blraZYSmB4Ozcl5697f4WwFbkalRtzPJu3ILqF5Z1UngvRtY1MqZynKedgUXQaAcRzfeqGXUDTQEnO1veGTxwnWPa1IqftDeJrppb2KVUb5SYfBlhW5FWrsYGjuBqemvxy78dqzO9suP';
+// const data = {
+//   currencyID: 3,
+//   amount: glideamount,
+//   recipientTypeID: 1,
+//   recipient: `${glideorder}`,
+//   clientOrderID: `${gliderecip}`,
+//   webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
+// };
 const data = {
-  currencyID: 3,
   amount: glideamount,
-  recipientTypeID: 1,
-  recipient: `${glideorder}`,
-  clientOrderID: `${gliderecip}`,
-  webhookURL: 'https://webhook.site/6c2fa6fa-ec9f-47ca-a2c2-18505a6dec9b'
-};
+  currencyID: 3,
+  typeID: 4,
+  clientOrderID: gliderecip,
+  TTL: 999,
+  webhookURL: "https://webhook.site/c7e3b35d-3e0b-42e7-a46b-e51b98e865c4",
+  extra: {
+    comment: "KrossBeri",
+    allowedPaymentTypeIDs: [
+      3
+    ],
+    allowedPaymentSystemIDs: [
+      1,
+      2
+    ],
+    allowedCardIssuerIDs: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8
+    ],
+    failedRedirectURL: "https://krossberi.glide.page/dl/failure",
+    successRedirectURL: "https://krossberi.glide.page/dl/success"
+  }
+}
 const raw = JSON.stringify(data);
 const hash = CryptoJS.HmacSHA512(raw, secret).toString();
 
